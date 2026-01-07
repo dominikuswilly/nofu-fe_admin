330e8916e26fe34b810cbf83e3083d9c43632ab3
willy <willy@kbru.co.id>
2026-01-07 12:43:21 +0700

    fix: stock initiation

--- src/modules/stock/views/Manage.vue
+++ src/modules/stock/views/Manage.vue
@@ -414,6 +414,7 @@ const parseJwt = (token: string) => {
     const segments = token.split('.');
     if (segments.length !== 3) return null;
     const base64Url = segments[1];
+    if (!base64Url) return null;
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
